61c452929e80389acd6c51657bb12f9f1563ab62
firman ramadhan <firmanramadhan066@gmail.com>
2024-11-13 11:29:31 +0700

    update

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $result = $db->query("SELECT * FROM books");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Peminjaman Buku</title>
-    <link rel="stylesheet" href="../peminjaman-buku-sekolah/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
